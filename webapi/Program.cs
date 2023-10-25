2127c1c4cded1c3d73404fee3d7d9127bba31f2b
if164ever1 <acere5ihor@gmail.com>
2023-10-25 21:19:51 +0200

    adjusting serilog

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,6 +1,22 @@
+using Serilog;
+using Serilog.Events;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var logger = new LoggerConfiguration()
+    .MinimumLevel.Debug().MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog();
+
+//SWAGER CONFIGURATION
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
