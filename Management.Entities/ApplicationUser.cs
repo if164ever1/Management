9697366230d56d47cd6cf464b7a2bd064af4a4a1
if164ever1 <acere5ihor@gmail.com>
2023-11-10 11:37:51 +0100

    reconstruction projects folders

---
+++ Management.Entities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+
+namespace Management.Entities
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public required string Name { get; set; }
+        public required string Avatar { get; set; }
+    }
+}
