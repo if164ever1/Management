1fdcb33bb47e6be4c5adcd409d790b512ecca871
if164ever1 <acere5ihor@gmail.com>
2023-11-10 10:56:52 +0100

    add dependences

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,13 +1,14 @@
 using Serilog;
 using Serilog.Events;
 
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var logger = new LoggerConfiguration()
-    .MinimumLevel.Debug().MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .ReadFrom.Configuration(builder.Configuration)
     .Enrich.FromLogContext()
-    .WriteTo.Console()
     .CreateLogger();
 
 builder.Logging.ClearProviders();
@@ -38,4 +39,31 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+
+app.MapGet("/weatherforecast", (ILogger<Program> logger) =>
+{
+    logger.LogDebug("Executing `/weatherforecast`");
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
 app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
